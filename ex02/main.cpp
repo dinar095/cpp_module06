53b9a83d343b8e34c515c243cef28433c4ee31af
Dinar Teregulov <dinar095@hotmail.com>
2021-10-04 09:18:14 +0300

    ex02 in progress

---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void)
+{
+
+}
+
+void identify(Base* p)
+{
+
+}
+
+void identify(Base& p)
+{
+
+}
+
+
+int main()
+{
+
+}
+
